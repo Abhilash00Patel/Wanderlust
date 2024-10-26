3ea51660725075126d2f7d20b5fddd46a6a2b802
Abhilash Patel <abhilashpatel49@gmail.com>
2024-10-27 04:29:50 +0530

    smallone

--- models/listing.js
+++ models/listing.js
@@ -3,13 +3,13 @@ const Review = require('./review');
 const {Schema} = mongoose
 
 
+const dbUrl = process.env.ATLASDB_URL
 
-// let MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust"
-// async function main() {
-//   await mongoose.connect(MONGO_URL);
-// }
-// main().then((res)=>{
-// }).catch(err => console.log(err));
+async function main() {
+  await mongoose.connect(dbUrl);
+}
+main().then((res)=>{
+}).catch(err => console.log(err));
 
 
   
